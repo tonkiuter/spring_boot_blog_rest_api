ae43feddc22f763f4b659384b2a97c941983064e
tonkiuter <aklivmairkoo@gmail.com>
2022-11-07 23:53:45 -0600

    Adding a Set of CommentsDTO to have in our response all comments from a Post

--- src/main/java/com/springboot/blog/payload/PostDTO.java
+++ src/main/java/com/springboot/blog/payload/PostDTO.java
@@ -3,11 +3,15 @@ package com.springboot.blog.payload;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class PostDTO {
     private Long Id;
     private String title;
     private String description;
     private String content;
+
+    private Set<CommentDTO> comments;
 
 }
